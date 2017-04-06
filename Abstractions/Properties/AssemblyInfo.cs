19612845291f06b65698bbda148850bb92009217
小朱 (Jeff Chu) <regionbbs@ms67.url.com.tw>
2017-04-06 18:59:09 +0800

    Remove package dependency temporary.

--- Abstractions/Properties/AssemblyInfo.cs
+++ Abstractions/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.22")]
-[assembly: AssemblyFileVersion("3.0.0.22")]
+[assembly: AssemblyVersion("3.0.0.23")]
+[assembly: AssemblyFileVersion("3.0.0.23")]
 [assembly: AssemblyInformationalVersion("3.0.0")]
--- Configuration.Json/Properties/AssemblyInfo.cs
+++ Configuration.Json/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.34")]
-[assembly: AssemblyFileVersion("3.0.0.34")]
+[assembly: AssemblyVersion("3.0.0.35")]
+[assembly: AssemblyFileVersion("3.0.0.35")]
 [assembly: AssemblyInformationalVersion("3.0.0")]
